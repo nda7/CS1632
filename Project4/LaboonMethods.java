bb8e9dfb43133020ee764c3241959e151429a1bf
nda7 <nda7@pitt.edu>
2016-07-05 13:14:12 -0400

    Added Comments

--- Project4/LaboonMethods.java
+++ Project4/LaboonMethods.java
@@ -1,7 +1,7 @@
 
 public class LaboonMethods {
 	
-	
+	//Test run to see method output
 	public static void main(String args[]){
 		int[] test = {1,2,3,4,5,6,7,8,9,10};
 		int[] finals = laboonify(test);
@@ -10,6 +10,10 @@ public class LaboonMethods {
 		}
 	}
 	
+	//Create a new array based off input array.
+	//The length will be x.length+1
+	//Each index i in the input array will be mapped to i^2 in the new array
+	//The last index in the new array will be the sum of the first length-1 indexes
 	public static int[] laboonify(int[] x){
 		int len = x.length;
 		int [] labooned = new int[len+1];
